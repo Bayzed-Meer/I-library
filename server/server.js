d16393b771244547543b3982677f56d9c31928de
Bayzed-Meer <rsbayzed@gmail.com>
2024-09-30 23:17:40 +0600

    style: add home page design

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DATABASE_URL, {});
