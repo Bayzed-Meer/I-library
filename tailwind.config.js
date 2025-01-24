58e66af9d2de8301c19302e424fbd6369f29460b
MD Bayzed Meer <bayzed.meer@brainstation-23.com>
2025-01-25 00:35:20 +0600

    refactor: tailwind color names

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,29 +9,37 @@ module.exports = {
         '8xl': '90rem',
       },
       colors: {
-        body: {
-          light: '#F9FAFB',
+        'body-primary': {
+          light: '#ffffff',
           dark: '#111827',
+        },
+        'body-secondary': {
+          light: '#f9fafb',
+          dark: '#1f2937',
         },
         container: {
           light: '#ffffff',
           dark: '#1F2937',
         },
-        heading: {
+        input: {
+          light: '#f9fafb',
+          dark: '#374151',
+        },
+        text: {
+          light: '#6b7280',
+          dark: '#9ca3af',
+        },
+        title: {
           light: '#111827',
           dark: '#ffffff',
         },
-        label: {
-          light: '#6b7280',
-          dark: '#9ca3af',
+        card: {
+          light: '#ffffff',
+          dark: '#374151',
         },
         primary: {
           light: '#1469C0',
           dark: '#9EBBF3',
-        },
-        'form-field-container': {
-          light: '#F9FAFB',
-          dark: '#374151',
         },
       },
     },
